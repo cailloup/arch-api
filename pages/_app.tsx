77778860d30aed6adf57a5753cd9af1d48f57899
cailloup <josevoyut@gmail.com>
2023-05-24 17:51:40 -0300

    agrego Sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '../styles/base.sass'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout'
 export default function App({ Component, pageProps }: AppProps) {
