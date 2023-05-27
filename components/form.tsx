8d529e1914895a3c63d424389e75363db14aa9cd
cailloup <josevoyut@gmail.com>
2023-05-27 18:20:10 -0300

    Update form.tsx

--- components/form.tsx
+++ components/form.tsx
@@ -3,13 +3,15 @@ import { Button, Input, Select } from './assests';
 import styles from '@/styles/form.module.sass'
 
 interface FormProps {
-  formComponents: FormComponent[];
+    submitText:string;
+    formComponents: FormComponent[];
 }
 
 export interface FormComponent {
     type: 'button' | 'input' | 'select';
     label: string;
     text?: string;
+    readonly?: boolean;
     buttonFeed?: string
     options?: string[];
     primary?: boolean
@@ -17,38 +19,62 @@ export interface FormComponent {
     onChange?: (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Form: React.FC<FormProps> = ({ formComponents }) => {
+function Form({ submitText, formComponents }: FormProps){
   return (
     <form>
         {formComponents.map((component, index) => (
-            <div key={index}>
+            <div key={index} >
                 <label className={styles.label}>{component.label} </label><br/>
-
-                {component.type === 'button' && (
-                <Button className={styles.margin} $primary={component.primary} onClick={component.onClick}>{component.text}</Button>
-                )}
+                <div className={styles.component}>
+                    {renderComponent(component)}
+                </div>
                 
-                {component.buttonFeed && (
-                    <label className={styles.label}> {component.buttonFeed} </label>
-                )}
-
-                {component.type === 'input' && (
-                <Input className={`${styles.input} ${styles.margin}`} onChange={component.onChange} placeholder={component.text} />
-                )}
-
-                {component.type === 'select' && (
-                <Select className={`${styles.input} ${styles.margin}`} onChange={component.onChange}>
-                    {component.options?.map(option => 
-                        <option key={option}>{option}</option>  
-                    )}
-                </Select>
-                )}
-
-                <br/><br/>
             </div>
         ))}
+        <Button $primary className='right'>{submitText}</Button>
     </form>
   );
 };
 
+const renderComponent = (component: FormComponent) => {
+    switch (component.type) {
+        case 'button':
+          return renderButton(component);
+        case 'input':
+          return renderInput(component);
+        case 'select':
+          return renderSelect(component);
+        default:
+          return null;
+      }
+}
+
+const renderButton = ({primary,onClick,text,buttonFeed}:FormComponent) =>{
+    return(
+        <>
+            <Button className={styles.margin} $primary={primary} onClick={onClick}>{text} </Button>
+            {buttonFeed && (
+                <> <br/> {buttonFeed} </>
+            )}
+        </>
+    )
+}
+
+const renderSelect = ({options,onChange}:FormComponent) =>{
+    return(
+        <>
+            <Select className={`${styles.input} ${styles.margin}`} onChange={onChange}>
+                {options?.map(option => 
+                    <option key={option}>{option}</option>  
+                )}
+            </Select>
+        </>
+    )
+}
+
+const renderInput = ({readonly,onChange,text}:FormComponent) =>{
+    return(
+        <Input readOnly={readonly} className={`${styles.input} ${styles.margin}`} onChange={onChange} placeholder={text} />
+    )
+}
 export default Form;
