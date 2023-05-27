3564a556c0d1d43edcabec4ffc9a9619b40d896e
cailloup <josevoyut@gmail.com>
2023-05-27 18:20:00 -0300

    Update formfields.ts

--- utils/formfields.ts
+++ utils/formfields.ts
@@ -1,22 +1,56 @@
 import { FormComponent } from "@/components/form";
 
+const buildingTypes = ["C. C. Municipal","Comercial","Educativo","Esparcimiento","Histórico","Hotelera","Municipal","Público","Religioso","Urbano","Vivienda","Otro"]
+const buildingStyles = ["Centro Europeo","Modernismo","Prefabricado","Tradicional","Otro"]
+
 export const formRegisterFields: FormComponent[] = [
     {
-      type: 'input',
-      text: 'Nombre del arquitecto',
-      label: 'Arquitecto'
+        type: 'input',
+        label: 'Partido',
+        readonly: true,
     },
     {
-      type: 'button',
-      text: 'seleccionar imagen',
-      label: 'Imagen edificio',
-      buttonFeed:'Imagen sin seleccionar',
-      primary: false,
+        type: 'input',
+        text: 'Direccion del edificio',
+        label: 'Direccion'
     },
     {
-      type: 'select',
-      label: 'Seleccione tipo de edificio',
-      options: ['palindromo','azucarado']
+        type: 'input',
+        text: 'Nombre del edificio',
+        label: 'Nombre'
     },
+    {
+        type: 'input',
+        text: 'Nombre del arquitecto',
+        label: 'Arquitecto'
+    },
+    {
+        type: 'input',
+        text: 'Estado del edificio',
+        label: 'Estado'
+    },
+    {
+        type: 'input',
+        text: 'Epoca del edificio',
+        label: 'Epoca'
+    },
+    {
+        type: 'button',
+        text: 'seleccionar  imagen',
+        label: 'Imagen edificio',
+        buttonFeed:'Imagen sin seleccionar',
+        primary: false,
+    },
+    {
+        type: 'select',
+        label: 'Seleccione tipo de edificio',
+        options: buildingTypes,
+    },
+    {
+        type: 'select',
+        label: 'Seleccione estilo del edificio',
+        options: buildingStyles,
+    },
+
   ]
   
