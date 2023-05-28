6e2e0cf09096e44539c6cb928fc94ca701d15d92
cailloup <josevoyut@gmail.com>
2023-05-28 16:27:43 -0300

    Update formfields.ts

--- utils/formfields.ts
+++ utils/formfields.ts
@@ -1,80 +1,21 @@
-import { FormComponent } from "@/components/form";
+import { ButtonComponent, Component, InputComponent, InputDateComponent, InputFileComponent, SelectComponent } from "@/components/FormComponents"
 
 const buildingTypes = ["C. C. Municipal","Comercial","Educativo","Esparcimiento","Histórico","Hotelera","Municipal","Público","Religioso","Urbano","Vivienda","Otro"]
 const buildingStyles = ["Centro Europeo","Modernismo","Prefabricado","Tradicional","Otro"]
 
-export const formRegisterFields: FormComponent[] = [
-    {
-        id:'county',
-        type: 'input&Button',
-        label: 'Partido',
-        readonly: true,
-        value: 'Partido de villa Gesell',
-        textButton:'cambiar',
-    },
-    {
-        id:'address',
-        type: 'input',
-        text: ['calle'],
-        label: 'Direccion'
-    },
-    {
-        id:'name',
-        type: 'input',
-        text: ['Nombre del edificio'],
-        label: 'Nombre'
-    },
-    {
-        id:'architect',
-        type: 'input',
-        text: ['Nombre','Apellido'],
-        label: 'Arquitecto'
-    },
-    {
-        id:'state',
-        type: 'input',
-        text: ['Estado del edificio'],
-        label: 'Estado'
-    },
-    {
-        id:'period',
-        type: 'input',
-        text: ['Epoca del edificio'],
-        label: 'Epoca'
-    },
-    {
-        id:'image',
-        type: 'inputFile',
-        text: ['seleccionar  imagen'],
-        label: 'Imagen edificio',
-        textButton: 'Seleccionar imagen',
-        primary: false,
-    },
-    {
-        id:'type',
-        type: 'select',
-        label: 'Seleccione tipo de edificio',
-        options: buildingTypes,
-    },
-    {
-        id:'style',
-        type: 'select',
-        label: 'Seleccione estilo del edificio',
-        options: buildingStyles,
-    },
-    {
-        id:'protected',
-        type: 'input',
-        text: ['Estado de proteccion'],
-        label: 'Proteccion',
-        optional: true,
-    },
-    {
-        id:'builtDate',
-        type: 'inputDate',
-        text: ['Fecha de construccion'],
-        label: 'Fecha de construccion',
-    },
-
-  ]
-  
+ export const formRegisterFields: Component[] = [
+        new InputComponent({id:'city',label:'Partido',readOnly:true,value:'Partido de villa gesell',placeHolder:['']}),
+        new ButtonComponent({id:'cambiar',onClick:()=>{},text:'Cambiar'}),
+        new InputComponent({id:'address',label:'Direccion',placeHolder:['Ingrese direcion del edificio']}),
+        new InputComponent({id:'name',label:'Nombre',placeHolder:['Ingrese nombre del edificio'],required:true}),
+        new InputComponent({id:'archytect',label:'Arquitecto',placeHolder:['Ingrese nombre del arquitecto','Ingrese apellido del arquitecto'] }),
+        new InputComponent({id:'state',label:'Estado',placeHolder:['Ingrese estado del edificio']}),
+        new InputComponent({id:'period',label:'Epoca',placeHolder:['Ingrese epoca de edificio']}),
+        new InputFileComponent({id:'image',label:'Imagen edificio',textButton:'Seleccionar imagen'}),
+        new SelectComponent({id:'type',label:'Tipo',options:buildingTypes}),
+        new SelectComponent({id:'style',label:'Estilo',options:buildingStyles}),
+        new InputComponent({id:'protected',label:'Proteccion',placeHolder:['Ingrese proteccion del edificio']}),
+        new InputDateComponent({id:'date',label:'Fecha de contruccion',min:'12-02-1800',max:new Date().toISOString().split("T")[0], defaultValue:new Date().toISOString().split("T")[0] }),
+    ]
+ 
+ 
