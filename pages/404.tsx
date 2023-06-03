d51d475d3de9bc4cc11feba0a6e88e24ce0027a3
cailloup <josevoyut@gmail.com>
2023-06-03 09:17:39 -0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,10 @@
 import { Container, ContainerColor } from "@/components/assets";
 
 export default function Custom404() {
-
     return (
-    <div style={{ height:'100%',display:'flex', justifyContent: 'center',alignItems: 'center'}}>
-       
-        <h1>  <ContainerColor $color="secondary"><i style={{fontSize:'522px'}} className="icon-charly"/></ContainerColor><br/>Ya ves que yo no te puedo dar,<br/>
-            las cosas que quisiste dejar</h1>
-    </div>)
-
+        <div style={{ height:'100%',display:'flex', justifyContent: 'center',alignItems: 'center'}}>
+        
+            <h1>  <ContainerColor $color="secondary"><i style={{fontSize:'522px'}} className="icon-charly"/></ContainerColor><br/>Ya ves que yo no te puedo dar,<br/>
+                las cosas que quisiste dejar</h1>
+        </div>)
   }
