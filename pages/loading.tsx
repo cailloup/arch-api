e0ce09539e0116a3b55a4293793108508e1a43f5
cailloup <josevoyut@gmail.com>
2023-06-03 09:18:38 -0300

    Update loading.tsx

--- pages/loading.tsx
+++ pages/loading.tsx
@@ -1,10 +1,9 @@
 import styles from "@/styles/loading.module.sass";
 export default function Loading() {
-    return (
-      <div className={styles.main}>
-        <div className={styles.loader}></div>
-        <h1 className={styles.loadText}>Cargando</h1>
-      </div>
-    )
-    ;
-  }
+  return (
+    <div className={styles.main}>
+      <div className={styles.loader}></div>
+      <h1 className={styles.loadText}>Cargando</h1>
+    </div>
+  )
+}
