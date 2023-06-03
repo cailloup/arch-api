3958bdef314c45475e25b4ecd77b5af4eaafab1a
cailloup <josevoyut@gmail.com>
2023-06-03 09:20:34 -0300

    Update adminBoard.tsx

--- pages/dashboard/adminBoard.tsx
+++ pages/dashboard/adminBoard.tsx
@@ -8,95 +8,95 @@ import styles from '@/styles/dashboard.module.sass'
 
 const api = new ArchytecstApi()
 const AdminBoard: React.FC<DashBoardProps> = ({ cleanList,clean,setSelectedBuilding,setShowModifyScreen,selectedBuilding, ...props }) => {
-    const [buildings,setBuildings] = useState<Building[]>([])
-    const [searchValue,setSearchValue] = useState<string>("")
-    const [selectedBuildings,setSelectedBuildings] = useState<Building[]>([])
+  const [buildings,setBuildings] = useState<Building[]>([])
+  const [searchValue,setSearchValue] = useState<string>("")
+  const [selectedBuildings,setSelectedBuildings] = useState<Building[]>([])
 
-    useEffect(() => {
-      toast.promise(
-        () => api.getBuildings(),
-        {
-          pending: 'Obteniendo edificios',
-          success: 'Edificios cargados correctamente 👌',
-          error: 'Hubo un error al cargar los edificios 🤯'
-        }
-      ).then( building => 
-        setBuildings(building)
-      )
+  useEffect(() => {
+    toast.promise(
+      () => api.getBuildings(),
+      {
+        pending: 'Obteniendo edificios',
+        success: 'Edificios cargados correctamente 👌',
+        error: 'Hubo un error al cargar los edificios 🤯'
+      }
+    ).then( building => 
+      setBuildings(building)
+    )
+    
+  }, [])
+  useEffect(() => {
+    if (!selectedBuilding)
+      return
+    if (!clean)
+      return
       
-    }, [])
-    useEffect(() => {
-      if (!selectedBuilding)
-        return
-      if (!clean)
-        return
-        
-      const updatedBuildings = buildings.map((building) => {
-        if ( building.uuid === selectedBuilding.uuid){
-          return selectedBuilding
+    const updatedBuildings = buildings.map((building) => {
+      if ( building.uuid === selectedBuilding.uuid){
+        return selectedBuilding
+      }
+      return building
+    })
+
+    setBuildings(updatedBuildings)
+    setSelectedBuildings([]);
+    cleanList()
+  }, [clean])
+  const fiteredBuildings = useMemo(()=>{  
+    return buildings.filter(building => building.name.toLowerCase().includes(searchValue.toLowerCase()))
+  },[buildings,searchValue]) 
+
+  const handleInputChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  }, []);
+
+  function hadleDelete(){
+    if (selectedBuildings.length==0){
+      toast.error('Error: debe seleccionar edificios para eliminar', {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+          return
+  }
+  selectedBuildings.forEach( ({uuid}) => {
+      deleteBuilding(uuid)
+  } )
+  }
+  function deleteBuilding(uuidDelete: string) {
+
+    toast.promise(
+        () => api.deleteBuilding(uuidDelete),
+        {
+          pending: 'Eliminando edificio',
+          success: 'Edificio eliminado correctamente 👌',
+          error: 'Hubo un error al eliminar el edificio 🤯'
         }
-        return building
-      })
-  
-      setBuildings(updatedBuildings)
-      setSelectedBuildings([]);
-      cleanList()
-    }, [clean])
-    const fiteredBuildings = useMemo(()=>{  
-      return buildings.filter(building => building.name.toLowerCase().includes(searchValue.toLowerCase()))
-    },[buildings,searchValue]) 
-
-    const handleInputChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
-      setSearchValue(e.target.value);
-    }, []);
-
-    function hadleDelete(){
-      if (selectedBuildings.length==0){
-        toast.error('Error: debe seleccionar edificios para eliminar', {
-            position: "top-center",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-            });
-            return
-    }
-    selectedBuildings.forEach( ({uuid}) => {
-        deleteBuilding(uuid)
-    } )
-    }
-    function deleteBuilding(uuidDelete: string) {
-
-      toast.promise(
-          () => api.deleteBuilding(uuidDelete),
-          {
-            pending: 'Eliminando edificio',
-            success: 'Edificio eliminado correctamente 👌',
-            error: 'Hubo un error al eliminar el edificio 🤯'
-          }
-        ).then(() => {
-          setBuildings(buildings.filter( ({uuid}) => uuid!=uuidDelete  ))
-          setSelectedBuildings([])
-        });
-    }
-    return (
-      <div {...props}>
-        <div className={styles.inputContainer}>
-          <Input placeholder='Ingrese nombre del edificio' onChange={handleInputChange}/>
-          <div>
-            <Button $primary disabled={selectedBuildings.length==0} onClick={hadleDelete} >  <i className="icon-bin"/> Eliminar</Button>
-            <Button  $primary disabled={(selectedBuildings.length!==1)} onClick={() => { if(setSelectedBuilding) setSelectedBuilding(selectedBuildings[0]);setShowModifyScreen()}} >Modificar</Button>
-          </div>
-        </div>
-
-        <div  className={styles.tableContainer}>   
-          <Table multiselect headers={headers} data={fiteredBuildings} setSelectData={setSelectedBuildings} selectedData={selectedBuildings}/>
+      ).then(() => {
+        setBuildings(buildings.filter( ({uuid}) => uuid!=uuidDelete  ))
+        setSelectedBuildings([])
+      });
+  }
+  return (
+    <div {...props}>
+      <div className={styles.inputContainer}>
+        <Input placeholder='Ingrese nombre del edificio' onChange={handleInputChange}/>
+        <div>
+          <Button $primary disabled={selectedBuildings.length==0} onClick={hadleDelete} >  <i className="icon-bin"/> Eliminar</Button>
+          <Button  $primary disabled={(selectedBuildings.length!==1)} onClick={() => { if(setSelectedBuilding) setSelectedBuilding(selectedBuildings[0]);setShowModifyScreen()}} >Modificar</Button>
         </div>
       </div>
-    )
+
+      <div  className={styles.tableContainer}>   
+        <Table multiselect headers={headers} data={fiteredBuildings} setSelectData={setSelectedBuildings} selectedData={selectedBuildings}/>
+      </div>
+    </div>
+  )
 }
   
 const headers:Header[] = [
